a16a3a8d3c7c71a3452c51f534ad2634ea5c32f5
yussarmvp69 <60917927+yussarmvp69@users.noreply.github.com>
2023-04-13 11:37:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!-- 
-  Anggota Kelompok
-  1. Yehezkiel David Setiawan (2172003) - Membuat halaman cover edit
-  2. Laurentius Gusti Ontoseno Panata Yudha (2172028) -  mengintegrasikan edit cover ketika buku baru ingin mengubah cover dan mendelete cover
-
-  Karena kami telah menyelesaikan cover edit pada akhir kelas, jadi kami hanya menambahkan beberapa syarat yang diperlukan saja.
-
--->
 
 <?php
   session_start();
